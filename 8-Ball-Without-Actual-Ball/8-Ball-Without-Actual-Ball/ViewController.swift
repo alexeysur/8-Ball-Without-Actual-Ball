96b0468a11eda8836fea4b3afb767fce66b91517
Alexey Sur <alexeysur@gmail.com>
2022-01-29 19:35:08 +0200

    init: create project in Xcode

---
+++ 8-Ball-Without-Actual-Ball/8-Ball-Without-Actual-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8-Ball-Without-Actual-Ball
+//
+//  Created by Alexey on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
