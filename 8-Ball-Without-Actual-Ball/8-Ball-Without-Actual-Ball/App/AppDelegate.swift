fe56f15bec3bcff6ce1745ce683c6933e6484a52
Alexey Sur <alexeysur@gmail.com>
2022-02-23 23:21:25 +0200

    add  Class Data for read/write plist-file

--- 8-Ball-Without-Actual-Ball/8-Ball-Without-Actual-Ball/App/AppDelegate.swift
+++ 8-Ball-Without-Actual-Ball/8-Ball-Without-Actual-Ball/App/AppDelegate.swift
@@ -9,38 +9,39 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    let tableAnswers = TableAnswers.tableObj
+    
+    let data = Data()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        readAnswersFromFile()
+        data.readAnswersFromFile()
         return true
     }
 
-    func readAnswersFromFile() {
-        
-        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
-        let documentsDirectory = paths.object(at: 0) as! NSString
-        let path = documentsDirectory.appendingPathComponent("Answers.plist")
-        
-        let fileManager = FileManager.default
-        if !fileManager.fileExists(atPath: path) {
-            guard let bundlePath = Bundle.main.path(forResource: "Answers", ofType: "plist") else { return }
-            do {
-                try fileManager.copyItem(atPath: bundlePath, toPath: path)
-            } catch let error as NSError {
-                print("Unable to copy file. ERROR: \(error.localizedDescription)")
-            }
-        }
-   
-        let myDict = NSDictionary(contentsOfFile: path)
-        if let dict = myDict {
-            //loading values
-            tableAnswers.arrayAnswers = dict.object(forKey: "Answers")! as! [String]
-            print("arrayAnswers = \(tableAnswers.arrayAnswers)")
-        } else {
-            print("WARNING: Couldn't create dictionary from Answers.plist!")
-        }
-    }
+//    func readAnswersFromFile() {
+//
+//        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
+//        let documentsDirectory = paths.object(at: 0) as! NSString
+//        let path = documentsDirectory.appendingPathComponent("Answers.plist")
+//
+//        let fileManager = FileManager.default
+//        if !fileManager.fileExists(atPath: path) {
+//            guard let bundlePath = Bundle.main.path(forResource: "Answers", ofType: "plist") else { return }
+//            do {
+//                try fileManager.copyItem(atPath: bundlePath, toPath: path)
+//            } catch let error as NSError {
+//                print("Unable to copy file. ERROR: \(error.localizedDescription)")
+//            }
+//        }
+//
+//        let myDict = NSDictionary(contentsOfFile: path)
+//        if let dict = myDict {
+//            //loading values
+//            tableAnswers.arrayAnswers = dict.object(forKey: "Answers")! as! [String]
+//            print("arrayAnswers = \(tableAnswers.arrayAnswers)")
+//        } else {
+//            print("WARNING: Couldn't create dictionary from Answers.plist!")
+//        }
+//    }
     
     
     // MARK: UISceneSession Lifecycle
@@ -48,10 +49,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
-
 }
 
---
+++ 8-Ball-Without-Actual-Ball/8-Ball-Without-Actual-Ball/App/Data.swift
@@ -0,0 +1,37 @@
+//
+//  Data.swift
+//  8-Ball-Without-Actual-Ball
+//
+//  Created by Alexey on 23.02.2022.
+//
+
+import UIKit
+
+final class Data {
+    let tableAnswers = TableAnswers.tableObj
+    
+    func readAnswersFromFile() {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
+        let documentsDirectory = paths.object(at: 0) as! NSString
+        let path = documentsDirectory.appendingPathComponent("Answers.plist")
+        
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: path) {
+            guard let bundlePath = Bundle.main.path(forResource: "Answers", ofType: "plist") else { return }
+            do {
+                try fileManager.copyItem(atPath: bundlePath, toPath: path)
+            } catch let error as NSError {
+                print("Unable to copy file. ERROR: \(error.localizedDescription)")
+            }
+        }
+   
+        let myDict = NSDictionary(contentsOfFile: path)
+        if let dict = myDict {
+            //loading values
+            tableAnswers.arrayAnswers = dict.object(forKey: "Answers")! as! [String]
+            print("arrayAnswers = \(tableAnswers.arrayAnswers)")
+        } else {
+            print("WARNING: Couldn't create dictionary from Answers.plist!")
+        }
+    }
+}
